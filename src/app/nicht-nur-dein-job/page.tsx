fb74a4f38560d190ccf99cb1bd4f0528d8d55cb1
mw3155 <mw3155@protonmail.com>
2026-06-04 22:26:13 +0200

    NNDJ: stimmen weiter vorne

--- src/app/nicht-nur-dein-job/page.tsx
+++ src/app/nicht-nur-dein-job/page.tsx
@@ -54,8 +54,8 @@ export default function NichtNurDeinJobPage() {
         <HeroSection description={DESCRIPTION} />
         <JumpBar />
         <JobLossCounterSection />
-        <ArbeitsmarktSection />
         <TestimonialsSection />
+        <ArbeitsmarktSection />
         <JobLossChartSection />
         <MehrAlsArbeitSection />
         <PolitischeForderungSection />
